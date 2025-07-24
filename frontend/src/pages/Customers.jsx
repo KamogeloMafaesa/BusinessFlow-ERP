2e13e2b0d18014956efd04683d3915a555e9a5f3
KamogeloMafaesa <kamogelomafaesa7@gmail.com>
2025-07-24 16:43:25 +0200

    added more buttons to the Customers page
    added the edit name button and another button to save the changes. I also added logic for making storage of data, to prevent data from getting lost when the page reloads

--- frontend/src/pages/Customers.jsx
+++ frontend/src/pages/Customers.jsx
@@ -1,12 +1,25 @@
 import React, {useState} from "react";
+import { useEffect } from "react";
 
-function Customers(){  
- const [customers, setCustomers] = useState([
+
+function Customers(){ 
+
+ const [customers, setCustomers] = useState(() =>{
+    const saved = localStorage.getItem("customers");
+    return saved ? JSON.parse(saved):[
     {id: 1, name: "Kamogelo Mafaesa"},
     {id: 2, name: "Farayi Mpofu"},
     {id: 3, name: "John Doe"},
- ]); /* useState will act as a box to store my customers, it start off empty*/
+    ]
+ }); /* useState will act as a box to store my customers, it start off empty*/
  const [name, setName] = useState("");
+
+ const [editingId, setEditingId] = useState(null);
+ const [editedName, setEditedName] = useState("");
+
+useEffect(() => {
+    localStorage.setItem("customers", JSON.stringify(customers));
+}, [customers]);
 
 //Adding customers logic
 const handleAddCustomer = (e) =>{
@@ -22,10 +35,31 @@ const handleAddCustomer = (e) =>{
     setName("");//clear input
 };
 
+
 //Deleting customers logic
 const handleDelete = (id) => {
     setCustomers((prev) => prev.filter((customer) => customer.id !== id));
-}
+};
+
+//start editing customer names logic
+
+const startEditing = (customer) => {
+    setEditingId(customer.id);
+    setEditedName(customer.name);
+};
+
+//Save edited customer name
+
+const handleSavedEdit = (id) => {
+    setCustomers((prev) =>
+    prev.map((customer) =>
+    customer.id === id ? {...customer, name: editedName } : customer
+    )
+    );
+    setEditingId(null);
+    setEditedName("");
+};
+
 
 return(
 
@@ -46,10 +80,45 @@ return(
         <ul className="space-y-2">
             {customers.map((customer) => (
                 <li key={customer.id} className="bg-white p-3 rounded shadow flex justify-between items-center">
+
+                    {editingId === customer.id ? (
+                        <>
+                        <input
+                        type ="text"
+                        value = {editedName}
+                        onChange = {(e) => setEditedName(e.target.value)}
+                        className = "border p-1 rounded mr-2"
+                        />
+
+                        <button onClick={() => handleSavedEdit(customer.id)}
+                            className="bg-green-500 text-white px-2 py-1 rounded hover:bg-green-600">
+                                Save
+                        </button>
+
+                        <button onClick={() => setEditingId(null)} 
+                         className="bg-gray-400 text-white px-2 py-1 rounded hover:bg-gray-500">
+                            Cancel
+                        </button>
+                        </>
+                    ):(
+                        <>
                    <span>{customer.name}</span>
-                   <button onClick={() => handleDelete(customer.id)} 
-                   className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600">
-                    Delete</button> 
+
+                   <div className="flex gap-2">
+
+
+                    <button onClick={() => startEditing(customer)}
+                     className="bg-yellow-500 text-white px-3 py-1 rounded hover:bg-yellow-600">
+                        Edit
+                    </button>
+
+                    <button onClick={() => handleDelete(customer.id)} 
+                     className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600">
+                    Delete
+                    </button> 
+                   </div>
+                    </>
+                    )}
                 </li>
             ))}
         </ul>
@@ -57,5 +126,4 @@ return(
     </div>
 );
 }
-
 export default Customers;

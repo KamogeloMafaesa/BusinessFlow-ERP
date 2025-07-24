a558c69f4705e5d6a865e3e8b96deaf3da55cf34
KamogeloMafaesa <kamogelomafaesa7@gmail.com>
2025-07-24 13:06:02 +0200

    added layout for the Dashboard/Home page
    added placeholders for dashboard stats

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {NavLink} from "react-router-dom"
 import { Link, Outlet} from "react-router-dom";
+import {Users, Home, FileText} from "lucide-react";
 
 function Layout(){
     return(
@@ -9,35 +10,39 @@ function Layout(){
             <aside className="w-64 bg-blue-800 text-white p-4 space-y-4">
                 <h1 className="text-2xl font-bold mb-6">Business Flow</h1>
                 <nav className="space-y-2">
+
                     <NavLink to="/" className={({ isActive }) =>
-                    `block px-4 py-2 hover:bg-blue-700 p-2 rounded text-white ${
-                    isActive ? "bg-blue-100 font-semibold text-blue-800 hover:bg-blue-100":
-                     "text-white"}`
+                    `flex items-center gap-2 px-4 py-2 rounded transition-colors duration-200 ${
+                    isActive ? "bg-blue-100 font-semibold text-blue-800":
+                     "text-white hover:bg-blue-700"}`
                      }
                      >
+                    <Home className="w-4-h-4"/>
                     Dashboard</NavLink>
                     
                     <NavLink
                      to="/customers"
                     className={({ isActive }) =>
-                     `block px-4 py-2 rounded hover:bg-blue-700 text-white ${
-                     isActive ? "bg-blue-100 font-semibold text-blue-800 hover:bg-blue-100" : 
-                     "text-white"
+                     `flex items-center gap-2 px-4 py-2 rounded transition-colors duration-200 ${
+                     isActive ? "bg-blue-100 font-semibold text-blue-800 " : 
+                     "text-white hover:bg-blue-700"
                     }`
                 }
             >
+            <Users className="w-4-h-4"/>
             Customers
                 </NavLink>
 
 
                     <NavLink to="/invoices" 
                     className={({ isActive }) =>
-                    `block px-4 py-2 hover:bg-blue-700 rounded text-white ${
-                        isActive ? "bg-blue-100 font-semibold text-blue-800 hover:bg-blue-100" :
-                        "text-white"
+                    `flex items-center gap-2 px-4 py-2  rounded transition-colors duration-200 ${
+                        isActive ? "bg-blue-100 font-semibold text-blue-800" :
+                        "text-white hover:bg-blue-700"
                     }`
                     }
                     >
+                    <FileText className="w-4-h-4"/>
                     Invoices</NavLink>
                 </nav>
             </aside>
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
 
 
-function Dashboard(){
+const Dashboard = () => {
+    return (
+        <div>
+            <h2 className="text-2xl font-bold text-gray-800 mb-4">Dashboard</h2>
+            <p className="text-gray-600 mb-6">Welcome to your ERP System.</p>
 
+            {/*Dashboard Cards */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                <div className="bg-white p-4 rounded shadow">
+                    <h3 className="text-lg font-semibold text-blue-800">Total Customers</h3>
+                    <p className="text-2xl font-bold text-gray-700">--</p>
+                </div>
+
+                <div className="bg-white p-4 rounded shadow">
+                    <h3 className="text-lg font-semibold text-blue-800">Invoices Isued</h3>
+                    <p className="text-2xl font-bold text-gray-700">--</p>
+                </div>
+
+                <div className="bg-white p-4 rounded shadow">
+                    <h3 className="text-lg font-semibold text-blue-800">Revenue</h3>
+                    <p className="text-2xl font-bold text-gray-700">R --</p>
+                </div>
+            </div>
+        </div>
+    )
 }
-
 export default Dashboard;
